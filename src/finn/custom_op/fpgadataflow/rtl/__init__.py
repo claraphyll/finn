690c2ee5a774dd463e86a33dd51196af6bc83185
clr <public@complexvector.space>
2025-11-17 19:07:26 +0100

    merge changes

--- src/finn/custom_op/fpgadataflow/rtl/__init__.py
+++ src/finn/custom_op/fpgadataflow/rtl/__init__.py
@@ -47,7 +47,11 @@ custom_op = dict()
 # make sure new HLSCustomOp subclasses are imported here so that they get
 # registered and plug in correctly into the infrastructure
 custom_op["ConvolutionInputGenerator_rtl"] = ConvolutionInputGenerator_rtl
+<<<<<<< Updated upstream
 custom_op["DeconvolutionInputGenerator_rtl"] = DeconvolutionInputGenerator_rtl
+=======
+custom_op["DeconvolutionInputGenerator_rtl"] = ConvolutionInputGenerator_rtl
+>>>>>>> Stashed changes
 custom_op["FMPadding_rtl"] = FMPadding_rtl
 custom_op["StreamingDataWidthConverter_rtl"] = StreamingDataWidthConverter_rtl
 custom_op["StreamingFIFO_rtl"] = StreamingFIFO_rtl

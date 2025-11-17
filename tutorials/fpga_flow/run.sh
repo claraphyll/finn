7e27b68ad1ee19df297d431571795ad1a22d4811
clr <public@complexvector.space>
2025-11-17 19:39:50 +0100

    update

--- tutorials/fpga_flow/run.sh
+++ tutorials/fpga_flow/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 export FINN_XILINX_PATH=/opt/vivado
 export FINN_XILINX_VERSION=2024.2
-export FINN_ROOT="$HOME/repos/finn"
+this_dir="$(dirname "$0")"
+export FINN_ROOT="$(realpath "$this_dir/../..")"
+echo $FINN_ROOT
 export FINN_BUILD_DIR="$FINN_ROOT/tutorials/fpga_flow/build"
 export PATH="$FINN_XILINX_PATH/Vitis_HLS/$FINN_XILINX_VERSION/bin:$PATH"
 export PATH="$FINN_XILINX_PATH/Vivado/$FINN_XILINX_VERSION/bin:$PATH"
